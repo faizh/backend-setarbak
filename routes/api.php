cd98332b55b1688db4695e450d3628427ad640fd
faizhermawan_ <faizhermawan13@gmail.com>
2022-11-29 00:19:21 +0700

    add api for menus

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/menu', [MenuController::class, 'index']);
+Route::get('/menu/beverages', [MenuController::class, 'getBeverages']);
+Route::get('/menu/foods', [MenuController::class, 'getFoods']);
+Route::get('/menu/{id}', [MenuController::class, 'show']);
