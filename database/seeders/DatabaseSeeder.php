44d2bb01a7eff627d325fcd6b58f3222cb8d7a56
faizhermawan_ <faizhermawan13@gmail.com>
2022-12-06 14:06:01 +0700

    add api to filter menus

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             // CategoriesSeeder::class,
-            MenusSeeder::class
+            // MenusSeeder::class
+            SubCategorySeeder::class
         ]);
     }
 }
